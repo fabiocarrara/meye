d44cdde47ea0f468a80b7ed1735baedf7ed11889
Fabio Carrara <fabio.carrara@isti.cnr.it>
2019-05-23 15:43:00 +0200

    added eye-blink tags & pupil xy estimation

--- dataloader.py
+++ dataloader.py
@@ -1,13 +1,26 @@
+import glob
+import os
+
 import keras
 import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
 from keras.preprocessing.image import load_img, img_to_array
+from scipy.ndimage import center_of_mass
 
 
 def load_xy(datum, x_shape, deterministic=True):
-    pupil_pos_yx = datum[['pupil-y', 'pupil-x']].values.astype(int)
-
+    y = load_img(datum.target)
+    if datum[['pupil-y', 'pupil-x']].isnull().values.any():
+        pupil_map = img_to_array(y)[:, :, 0]  # R channel = pupil
+        with np.errstate(invalid='raise'):
+            try:
+                pupil_pos_yx = center_of_mass(pupil_map)
+            except:
+                pupil_pos_yx = (x_shape[0] / 2, x_shape[1] / 2)
+    else:
+        pupil_pos_yx = datum[['pupil-y', 'pupil-x']].values.astype(int)
+    
     # new pupil position: we want it to be in [0.1, 0.9] (in percentage height and width) 95% of times
     pupil_new_pos = np.array([.5, .5]) if deterministic else np.random.normal(loc=0.5, scale=0.2, size=2)
 
@@ -18,23 +31,30 @@ def load_xy(datum, x_shape, deterministic=True):
     oy, ox = pupil_pos_yx - pupil_new_pos_yx
 
     crop = (ox, oy, ox + s, oy + s)  # Left, Upper; Right, Lower
-    x = load_img(datum.filename, color_mode='grayscale').crop(crop).resize(
-        x_shape[:2])  # TODO: check interpolation type
+    x = load_img(datum.filename, color_mode='grayscale').crop(crop).resize(x_shape[:2])  # TODO: check interpolation type
     x = img_to_array(x) / 255.0
 
-    y = load_img(datum.target).crop(crop).resize(x_shape[:2])
+    y = y.crop(crop).resize(x_shape[:2])
     y = img_to_array(y)[:, :, :2] / 255.0  # keep only red and green channels
+    
+    is_pupil_present = y[:, :, 0].sum() > 0
+    if datum.eye & ~datum.blink:
+        datum.eye = is_pupil_present
+        
+    y2 = datum[['eye', 'blink']]
 
-    return pd.Series({'x': x, 'y': y})
+    return pd.Series({'x': x, 'y': y, 'y2': y2})
 
 
 class DataGen(keras.utils.Sequence):
 
-    def __init__(self, data, data_dir, x_shape, batch_size, deterministic=False):
+    def __init__(self, data, data_dir=None, x_shape=(128, 128, 1), batch_size=8, deterministic=False):
         self.data = data.copy()
-        self.data['target'] = data_dir + '/pngs/' + self.data.filename.str.replace('jpeg', 'png')
-        self.data['filename'] = data_dir + '/fullFrames/' + self.data.filename
         self.data_dir = data_dir
+        
+        if data_dir is not None:
+            self.data['target'] = data_dir + '/annotation/png/' + self.data.filename.str.replace(r'jpe?g', 'png')
+            self.data['filename'] = data_dir + '/fullFrames/' + self.data.filename
 
         self.x_shape = x_shape
         self.batch_size = batch_size
@@ -48,10 +68,26 @@ class DataGen(keras.utils.Sequence):
         batch_xy = self.batch.apply(lambda row: load_xy(row, self.x_shape, deterministic=self.deterministic), axis=1)
         batch_x = np.stack(batch_xy.x)
         batch_y = np.stack(batch_xy.y)
-        return batch_x, batch_y
+        batch_y2 = np.stack(batch_xy.y2)
+        
+        return batch_x, [batch_y, batch_y2]
+
+
+def load_datasets(dataset_dirs):
+
+    def _load_and_prepare_annotations(dataset_dir):
+        data = os.path.join(dataset_dir, 'annotation', 'annotations.csv')
+        data = pd.read_csv(data)
+        data['target'] = dataset_dir + '/annotation/png/' + data.filename.str.replace(r'jpe?g', 'png')
+        data['filename'] = dataset_dir + '/fullFrames/' + data.filename
+        return data
+
+    dataset = pd.concat([_load_and_prepare_annotations(d) for d in dataset_dirs])
+    return dataset
 
 
 if __name__ == '__main__':
+
     x = plt.imread('data/NN_fullframe/fullFrames/y-00B-1-0.jpeg')
     y = plt.imread('data/NN_fullframe/pngs/y-00B-1-0.png')
 
