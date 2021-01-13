b0b0e70d6d77169870c8481355a7bbee6ae96504
Fabio Carrara <fabio.carrara@isti.cnr.it>
2021-01-13 21:26:39 +0100

    refactored prediction script, removed cv2

--- predict.py
+++ predict.py
@@ -1,71 +1,71 @@
 import argparse
 
-import cv2
+import imageio
 import numpy as np
+
 from keras.models import load_model
-
+from PIL import Image, ImageOps
 from tqdm import tqdm
-from utils import visualizable, compute_metrics
-
-
-def predictions(model, cap, process=None):
-    while cap.isOpened():
-        ret, frame = cap.read()
-        if not ret:
-            break
-
-        if process:
-            frame = process(frame)
-        frame = frame[:, :, None]  # same as np.expand_dims
-        pred = model.predict(frame[None, ...])
-        yield frame, pred
-
-    cap.release()
+from utils import draw_predictions, compute_metrics
 
 
 def main(args):
-
-    # x_shape = (args.rh, args.rw, 1)
-    # y_shape = (args.rh, args.rw, 2)
+    video = imageio.get_reader(args.video)
+    n_frames = video.count_frames()
+    fps = video.get_meta_data()['fps']
+    frame_w, frame_h = video.get_meta_data()['size']
 
     # model = build_model(x_shape, y_shape)
     # model.load_weights('best_weights.hdf5')
     model = load_model(args.model)
-    cap = cv2.VideoCapture(args.video)
-
-    n_frames = int(cap.get(cv2.CAP_PROP_FRAME_COUNT))
-    fps = cap.get(cv2.CAP_PROP_FPS)
-    # interval = 1000 / fps
+    input_shape = model.input.shape[1:3]
 
     # y, x, h, w = (51, 70, 128, 128)  # RoI
-    def roi(frame):
-        eye = frame[args.ry:args.ry + args.rh, args.rx:args.rx + args.rw]
-        eye = cv2.cvtColor(eye, cv2.COLOR_BGR2GRAY)
-        eye = eye.astype(np.float32) / 255.0
+    if None in (args.rl, args.rt, args.rr, args.rb):
+        side = min(frame_w, frame_h)
+        args.rl = (frame_w - side) / 2
+        args.rt = (frame_h - side) / 2
+        args.rr = (frame_w + side) / 2
+        args.rb = (frame_h + side) / 2
+
+    crop = (args.rl, args.rt, args.rr, args.rb)
+
+    def preprocess(frame):
+        frame = Image.fromarray(frame)
+        eye = frame.crop(crop)
+        eye = ImageOps.grayscale(eye)
+        eye = eye.resize(input_shape)
         return eye
 
-    fourcc = cv2.VideoWriter_fourcc(*'MJPG')
-    out = cv2.VideoWriter(args.output, fourcc, fps, (args.rh, args.rw))
+    def predict(eye):
+        eye = np.array(eye).astype(np.float32) / 255.0
+        eye = eye[None, :, :, None]
+        return model.predict(eye)
 
-    for frame, pred in tqdm(predictions(model, cap, roi), total=n_frames):
-        (maps,), ((eye, blink),) = pred
-        img = visualizable(frame, maps)
-        img = (img[:, :, ::-1] * 255).astype(np.uint8)  # RGB float -> BGR uint8
-        cv2.putText(img, 'E: {:3.1%}'.format(eye), (5, args.rh - 20), cv2.FONT_HERSHEY_SIMPLEX, .4, (255, 0, 0))
-        cv2.putText(img, 'B: {:3.1%}'.format(blink), (5, args.rh - 5), cv2.FONT_HERSHEY_SIMPLEX, .4, (255, 0, 0))
-        out.write(img)
+    out = imageio.get_writer(args.output, fps=fps)
 
-    out.release()
+    cropped = map(preprocess, video)
+    frames_and_predictions = map(lambda x: (x, predict(x)), cropped)
+
+    for frame, predictions in tqdm(frames_and_predictions, total=n_frames):
+        img = draw_predictions(frame, predictions, thr=args.thr)
+        img = np.array(img)
+        out.append_data(img)
+
+    out.close()
 
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Predict on test video')
     parser.add_argument('model', type=str, help='Path to model')
     parser.add_argument('video', type=str, help='Video file to process')
-    parser.add_argument('-ry', type=int, default=0, help='RoI Y coordinate of top left corner')
-    parser.add_argument('-rx', type=int, default=0, help='RoI X coordinate of top left corner')
-    parser.add_argument('-rh', type=int, default=128, help='RoI height (-1 for full height)')
-    parser.add_argument('-rw', type=int, default=128, help='RoI width (-1 for full width)')
+
+    parser.add_argument('-t', '--thr', type=float, default=0.5, help='Map Threshold')
+    parser.add_argument('-rl', type=int, help='RoI X coordinate of top left corner')
+    parser.add_argument('-rt', type=int, help='RoI Y coordinate of top left corner')
+    parser.add_argument('-rr', type=int, help='RoI X coordinate of right bottom corner')
+    parser.add_argument('-rb', type=int, help='RoI Y coordinate of right bottom corner')
+
     parser.add_argument('-o', '--output', default='predictions.mp4', help='Output video')
 
     args = parser.parse_args()
