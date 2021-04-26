474f6e1e1736752699ad408d04a6d6ea8e4b8c32
Fabio Carrara <fabio.carrara@isti.cnr.it>
2021-04-26 11:30:32 +0200

    fixed unloadable keras model

--- models/deeplab_resnet50.py
+++ models/deeplab_resnet50.py
@@ -3,6 +3,7 @@ sys.path += ['models/deeplab']
 
 import tensorflow as tf
 
+from tensorflow.keras import backend as K
 from tensorflow.keras import layers as L
 from tensorflow.keras.models import Model, Sequential
 
@@ -17,7 +18,8 @@ def build_model(input_shape, output_shape, config):
     # backbone pretends RGB images to use pretrained weights
     needs_rgb_conversion = input_shape[2] != 3
     backbone_input_shape = (input_shape[:2] + (3,)) if needs_rgb_conversion else input_shape
-    backbone, backbone_len = Deeplabv3pResNet50(input_shape=backbone_input_shape, num_classes=num_classes, weights='imagenet', OS=8)
+    weights = config.get('weights', 'imagenet')
+    backbone, backbone_len = Deeplabv3pResNet50(input_shape=backbone_input_shape, num_classes=num_classes, weights=weights, OS=8)
 
     # segmentation mask
     out_mask = backbone.get_layer('pred_resize').output
@@ -32,19 +34,19 @@ def build_model(input_shape, output_shape, config):
 
     if needs_rgb_conversion:
         gray_input = L.Input(shape=input_shape)
-        rgb_input = L.Lambda(tf.image.grayscale_to_rgb)(gray_input)
+        rgb_input = L.Lambda(lambda x: K.tile(x, (1, 1, 1, 3)) , name='gray2rgb')(gray_input)  # we assume BHWC
         out_mask, out_tags = model(rgb_input)
 
         # rename outputs
         out_mask = L.Lambda(lambda x: x, name='mask')(out_mask)
         out_tags = L.Lambda(lambda x: x, name='tags')(out_tags)
         model = Model(inputs=gray_input, outputs=[out_mask, out_tags])
-    
+
     return model
 
 
 if __name__ == "__main__":
     shape = (128, 128, 1)
-    model = build_model(shape, shape, {'aspp': True})
+    model = build_model(shape, shape, {'weights': None})
     model.summary()
     import pdb; pdb.set_trace()
